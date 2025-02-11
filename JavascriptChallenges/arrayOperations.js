52484dbe1567f398b90e0cc1aea3bcd063128e5b
asaudu <adaoraa.91@gmail.com>
2025-02-11 14:32:09 -0600

    add an item at a specific index

--- JavascriptChallenges/arrayOperations.js
+++ JavascriptChallenges/arrayOperations.js
@@ -8,3 +8,19 @@ function a(arr, num){
         }
     }
 }
+
+//Add an item to an array at specific index and return new array (splice)
+function b(arr, item, index) {
+    //create a variable that holds the sliced array up until the desired index
+    //copy item into that new variable
+    //then copy the remainder of the initial array
+    //return new array
+
+    let newHotness = arr.slice(0,index);
+    newHotness.push(item);
+    let endHotness = arr.slice(index + 1);
+    let finalArr = [...newHotness, ...endHotness];
+
+    return finalArr;
+
+}
