50a3e151fa1ce148f89547234de539a2a6c15edf
asaudu <adaoraa.91@gmail.com>
2025-03-05 16:35:52 -0600

    adding boiler code and pseudo

--- JavascriptChallenges/betweenTwoSets.js
+++ JavascriptChallenges/betweenTwoSets.js
@@ -53,3 +53,74 @@ Explanation
 4, 8 and 16 are the only three numbers for which each element of a is a factor
 and each is a factor of all elements of b. 
 */
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function getTotalX(a, b) {
+    // Write your code here
+    /*
+    declare a counter var to hold integers between the sets
+    iterate over the first array
+        use the highest value in the first arr to determine the divisibility of all elements
+        if nothing is, or if one element is not, then return 0
+    if the remainder is 0 for all elements
+        iterate over the second array    
+            use the highest value of arr1 and the lowest value (aka first value) of arr2 and divide
+                if there is a remainder, return 0
+            if not a remainder
+                increment the counter by 1
+    return the counter      
+     */
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const brr = readLine().replace(/\s+$/g, '').split(' ').map(brrTemp => parseInt(brrTemp, 10));
+
+    const total = getTotalX(arr, brr);
+
+    ws.write(total + '\n');
+
+    ws.end();
+}
