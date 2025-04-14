ceb784abe017397c5c15bc641e5b50e505f61844
asaudu <adaoraa.91@gmail.com>
2025-04-14 12:59:19 -0500

    boiler plate code

--- JavascriptChallenges/extraLongFactorials.js
+++ JavascriptChallenges/extraLongFactorials.js
@@ -22,3 +22,41 @@ SamplenOutput
 15511210043330985984000000
 25! = 25 x 24 x 23 x ... x 3 x 2 x 1
 */
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'extraLongFactorials' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function extraLongFactorials(n) {
+    // Write your code here
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    extraLongFactorials(n);
+}
