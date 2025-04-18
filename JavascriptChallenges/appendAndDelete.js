d35edda3f319ed115271d189a91f04087fb71f38
asaudu <adaoraa.91@gmail.com>
2025-04-17 19:25:55 -0500

    adding boiler plate code

--- JavascriptChallenges/appendAndDelete.js
+++ JavascriptChallenges/appendAndDelete.js
@@ -81,3 +81,57 @@ Explanation 2
 
 To convert ashley to ash a minimum of 3 steps are needed. Hence we print No as answer. 
 */
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'appendAndDelete' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. STRING t
+ *  3. INTEGER k
+ */
+
+function appendAndDelete(s, t, k) {
+    // Write your code here
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const t = readLine();
+
+    const k = parseInt(readLine().trim(), 10);
+
+    const result = appendAndDelete(s, t, k);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
