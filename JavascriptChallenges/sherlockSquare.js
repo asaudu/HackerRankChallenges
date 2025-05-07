37563eaa1aa01a9920cf5eba5d7149418c6188c3
asaudu <adaoraa.91@gmail.com>
2025-05-06 19:07:54 -0500

    added boiler plate code and first pass

--- JavascriptChallenges/sherlockSquare.js
+++ JavascriptChallenges/sherlockSquare.js
@@ -43,3 +43,80 @@ Explanation
 Test Case #00: In range [3, 9], 4 and 9 are the two square integers.
 Test Case #01: In range [17, 24], there are no square integers. 
 */
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'squares' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER a
+ *  2. INTEGER b
+ */
+
+function squares(a, b) {
+    // Write your code here
+    /*
+    looking for values that are the result of a square
+    [3, 9] = 3, 4, 5, 6, 7, 8, 9
+    a var to be the counter to return
+    a loop starting at a and ending at b
+    with each iteration find the square root of the value at i
+    if the value has no remainder, increment the counter
+    return the counter
+     */
+    
+    let counter = 0;
+    
+    for(let i = a; i <= b; i++) {
+        if(Math.sqrt(i) % 1 === 0) {
+            counter++;
+        }
+    }
+    
+    return counter;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const a = parseInt(firstMultipleInput[0], 10);
+
+        const b = parseInt(firstMultipleInput[1], 10);
+
+        const result = squares(a, b);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
