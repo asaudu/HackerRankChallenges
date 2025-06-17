7c885a09df682e600f35d669c9fc4639bc7686eb
asaudu <adaoraa.91@gmail.com>
2025-06-16 21:42:25 -0500

    solution updated to resolve timeout issue

--- JavascriptChallenges/climbingTheLeaderboard.js
+++ JavascriptChallenges/climbingTheLeaderboard.js
@@ -102,3 +102,78 @@ After Alice finishes game , her score is  and her ranking is :
 
 image
 */
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'climbingLeaderboard' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY ranked
+ *  2. INTEGER_ARRAY player
+ */
+
+function climbingLeaderboard(ranked, player) {
+    // Write your code here
+    let uniqueRanked = [];
+    for (let u = 0; u < ranked.length; u++) {
+    if (ranked[u] !== ranked[u + 1]) {
+        uniqueRanked.push(ranked[u]);
+        }
+    }
+
+    let playerRanks = [];
+    let i = uniqueRanked.length - 1;
+
+    for(let p = 0; p < player.length; p++) {
+
+        while (i >= 0 && player[p] >= uniqueRanked[i]) {
+            i--;
+        }
+
+        playerRanks.push(i + 2);
+    }
+    return playerRanks;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const rankedCount = parseInt(readLine().trim(), 10);
+
+    const ranked = readLine().replace(/\s+$/g, '').split(' ').map(rankedTemp => parseInt(rankedTemp, 10));
+
+    const playerCount = parseInt(readLine().trim(), 10);
+
+    const player = readLine().replace(/\s+$/g, '').split(' ').map(playerTemp => parseInt(playerTemp, 10));
+
+    const result = climbingLeaderboard(ranked, player);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
